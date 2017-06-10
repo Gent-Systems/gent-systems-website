2f665097afdf368c1f56fd1291d08e559df051aa
John Gentile <johncgentile17@gmail.com>
2017-06-10 12:44:27 -0500

    edits to hyperlinks

--- scripts/ie10-viewport-bug-workaround.js
+++ scripts/ie10-viewport-bug-workaround.js
@@ -3,13 +3,8 @@
  * Copyright 2014-2015 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
-
-// See the Getting Started docs for more information:
-// http://getbootstrap.com/getting-started/#support-ie10-width
-
 (function () {
   'use strict';
-
   if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
     var msViewportStyle = document.createElement('style')
     msViewportStyle.appendChild(
@@ -19,5 +14,4 @@
     )
     document.querySelector('head').appendChild(msViewportStyle)
   }
-
 })();
